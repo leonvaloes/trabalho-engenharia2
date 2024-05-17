30f35869b51fe3bba37912298aa38d143685f742
Leon Valões <leovaloesnovaes@gmail.com>
2024-05-17 20:28:54 -0300

    Salvando mudanças locais antes de reverter o merge

--- back-end/src/main/java/com/backend/com/backend/model/repositorios/ModelTipoProdutosRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.backend.com.backend.model.repositorios;
-
-import com.backend.com.backend.model.entidades.TipoProdutos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModelTipoProdutosRepositorio extends JpaRepository<TipoProdutos,Long> {
-}
