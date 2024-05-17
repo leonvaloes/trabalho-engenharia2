317caec360adaa89946afffb9e792a5e148d1f4d
Felipe Huss <felipehuss03@gmail.com>
2024-05-17 17:59:48 -0300

    terminei meu crud de produtos e adicionei um controller para usar meus produtos, que é o que eu acho que ta certo

--- back-end/src/main/java/com/backend/com/backend/model/repositorios/ModelProdutosRepositorio.java
+++ back-end/src/main/java/com/backend/com/backend/model/repositorios/ModelProdutosRepositorio.java
@@ -3,5 +3,10 @@ package com.backend.com.backend.model.repositorios;
 import com.backend.com.backend.model.entidades.ProdutosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ModelProdutosRepositorio extends JpaRepository<ProdutosModel,Long> {
+
+    List<ProdutosModel> findAllByTipoProdutosId(Long id);
+
 }
